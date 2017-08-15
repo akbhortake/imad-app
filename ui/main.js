81afd8e37723b81290d1231314b366679886f817
akbhortake <akbhortake@gmail.com>
2017-08-15 14:12:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 console.log('Loaded!');
 var count=0;
 var btn=Doucument.getElementById("btn");
-var span=Doucument.getElementById("showcount")
 btn.onClick()
 {
+    var span=Doucument.getElementById("showcount")
     count=1+count;
     span.innerHTML(count.toStirng());
 }
