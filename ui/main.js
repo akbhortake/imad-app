39d319c63d9baabe895d65d7e72dd9f70d5b7637
akbhortake <akbhortake@gmail.com>
2017-08-15 14:30:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 console.log('Loaded!');
 var count=0;
 alert("asdf");
-var btn=doucument.getElementById("btn");
+var btn=document.getElementById("btn");
 btn.onClick = function ()
 {
     alert("hi");
-    var span=doucument.getElementById("showcount");
+    var span=document.getElementById("showcount");
     count=1+count;
     span.innerHTML("any");
 };
