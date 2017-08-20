b883389d301a4c9bf5fe6448113889d0a5691185
akbhortake <akbhortake@gmail.com>
2017-08-20 13:18:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,10 +5,37 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var Pool=require('pg').Pool;
+
+var config=
+{
+    user:'akbhortake',
+    database:'akbhortake',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    
+};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool=new Pool(config);
+
+app.get('/testdb', function (req, res) {
+  pool.query('SELECT * FORM sampledb',function(err,result){
+     if(err)
+     {
+         
+     } 
+     else
+     {
+         res.send(JSON.stringify(result));
+     }
+  });
+});
+
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
